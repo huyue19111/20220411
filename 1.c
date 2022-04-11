9ddd96d9004fc375ef8180b823f701d8034dc7a4
huiyue19111 <1161391384@qq.com>
2022-04-11 14:38:38 +0800

    1.c-1

--- 1.c
+++ 1.c
@@ -5,5 +5,7 @@ int main(int argc, const char *argv[])
 	printf("hello world\n");
 	printf("hello huyue\n");
 	printf("hello develop\n");
+	printf("hello main\n");
+	printf("hello 11111\n");
 	return 0;
 }
